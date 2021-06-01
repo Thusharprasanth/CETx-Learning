149f813af15c58951b02b081c9e8f0cfbad4efb8
thushar prasanth <thusharprasanth@gmail.com>
2021-06-01 23:31:03 +0530

    store page enabled

--- cetex/views.py
+++ cetex/views.py
@@ -1,11 +1,10 @@
 from courses.models import Course
-from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def store(request):
+def index(request):
     courses = Course.objects.all()
     context = {
         'courses' : courses
     }
-    return render(request , 'store/store.html', context)
+    return render(request , 'index.html', context)
